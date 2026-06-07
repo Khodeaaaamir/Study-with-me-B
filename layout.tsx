50c2ff86ab9b0622fa14302e259832859121eaa7
Khodeaaaamir <ghavidelamirmahdi@gmail.com>
2026-06-07 04:18:33 +0330

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif", background: "#0b0b10", color: "white" }}>
+        {children}
+      </body>
+    </it
